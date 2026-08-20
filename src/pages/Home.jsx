f51903641577e8640a355c64d3753e7705508a96
Yasir Rajput <rajputyasir2005@gmail.com>
2026-08-20 18:22:24 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import HeroSection from "../components/UI/HeroSection";
+import About from "./About";
+
+const Home = () => {
+  return (
+    <>
+      <HeroSection />
+      <About />
+    </>
+  );
+};
+
+export default Home;
